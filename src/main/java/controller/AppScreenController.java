2f5c1f85b624074228648b5d58246cad073bd0ad
aakanksha1998 <apatel439@gatech.edu>
2016-09-26 18:43:01 -0400

    Better implementation of the logout button
    changed the logout action to confirm logout.

--- src/main/java/controller/AppScreenController.java
+++ src/main/java/controller/AppScreenController.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import main.MainFXApplication;
+import javafx.scene.control.ButtonType;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Created by ahjin on 9/22/2016.
@@ -19,7 +21,15 @@ public class AppScreenController {
     @FXML
     private void handleButtonClicked(ActionEvent event) throws IOException {
         if (event.getSource() == logout) {
-            mainApplication.showWelcomeScreen();
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirm Logout");
+            alert.setHeaderText("Are you sure you want to logout?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                mainApplication.showWelcomeScreen();
+            } else {
+                alert.close();
+            }
         }
     }
     // Give the controller access to the main app.
